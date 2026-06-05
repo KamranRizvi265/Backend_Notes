305e216d9710aa079f3bd44d4b5037ab97a88ba1
KamranRizvi265 <kamranrizvicadet@gmail.com>
2026-06-05 10:27:21 +0530

    Updated contact.html

--- Flask_Projects/Blog/main.py
+++ Flask_Projects/Blog/main.py
@@ -3,8 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def home():
+def home_default():
     return render_template('index.html')
+
+@app.route("/index.html")
+def home_nav():
+    return render_template('index.html')
+
 
 
 @app.route("/about.html")
