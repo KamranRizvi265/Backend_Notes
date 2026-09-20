fdd9893752803aa499c6e67bfdaa00744e98a934
KamranRizvi265 <kamranrizvicadet@gmail.com>
2026-09-20 21:09:50 +0530

    django templates

--- django-codewithharry/textutils/textutils/views.py
+++ django-codewithharry/textutils/textutils/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, World!")
+    params = {"name": "John", "place": "New York"}
+    return render(request, "index.html", params)
 
 def about(request):
     return HttpResponse("This is the about page.")
