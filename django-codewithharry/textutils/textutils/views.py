aa79c8e787b5383d1cf689b31c87c32a0443c95e
KamranRizvi265 <kamranrizvicadet@gmail.com>
2026-09-22 12:08:40 +0530

    Punctuation remover

--- django-codewithharry/textutils/textutils/views.py
+++ django-codewithharry/textutils/textutils/views.py
@@ -7,3 +7,18 @@ def index(request):
 
 def about(request):
     return HttpResponse("This is the about page.")
+
+def analyze(request):
+    djtext = request.GET.get("text", "default")
+    removepunc = request.GET.get("removepunc", "off")
+    print(removepunc)
+    if removepunc == "on":
+        punctuations = '''!()-[]{};:'",<>./?@#$%^&*_~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
+        params = {"purpose": "Removed Punctuations", "analyzed_text": analyzed}
+        return render(request, "analyze.html", params)
+    else:
+        return HttpResponse("Error")
